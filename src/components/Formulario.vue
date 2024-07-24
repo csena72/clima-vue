ffd1f11bbfbd6e8fc4e5321d9cae91651f1e599b
Cristian Sena <crisianjsena@gmail.com>
2024-07-23 21:41:13 -0300

    Agrego el formulario

---
+++ src/components/Formulario.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { reactive, ref } from 'vue'
+import Alerta from './Alerta.vue'
+
+    const busqueda = reactive({
+        ciudad: '',
+        pais: ''
+    })
+
+    const error = ref('')
+
+    const paises = [
+        { codigo: 'US', nombre: 'Estados Unidos' },
+        { codigo: 'MX', nombre: 'México' },
+        { codigo: 'AR', nombre: 'Argentina' },
+        { codigo: 'CO', nombre: 'Colombia' },
+        { codigo: 'CR', nombre: 'Costa Rica' },
+        { codigo: 'ES', nombre: 'España' },
+        { codigo: 'PE', nombre: 'Perú' }
+]
+
+    const consultarClima = () => {
+
+        if (Object.values(busqueda).includes('')) {
+            error.value = 'Todos los campos son obligatorios'
+            setTimeout(() => {
+                error.value = ''
+            }, 3000);
+            return
+        }
+        console.log('Busqueda...')
+    }
+
+</script>
+
+<template>
+    <form 
+        class="formulario"
+        @submit.prevent="consultarClima"
+    >
+    <Alerta v-if="error">{{error}}</Alerta> 
+        
+        <div class="campo">
+            <label for="ciudad">Ciudad</label>
+            <input 
+                type="text"
+                id="ciudad"
+                placeholder="Ciudad..."
+                v-model="busqueda.ciudad"
+            />
+        </div>
+        <div class="campo">
+            <label for="País">País</label>
+            <select
+                id="pais"
+                name="pais"
+                v-model="busqueda.pais"
+            >
+                <option value="">-- seleccione un País --</option>
+                <option 
+                    v-for="pais in paises"
+                    :key="pais.codigo"
+                    :value="pais.codigo"
+                >{{pais.nombre}}</option>
+            </select>
+        </div>
+        <input type="submit" value="Consultar Clima">
+    </form>
+</template>
+
